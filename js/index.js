198b4828c468d8d515549fc1554e9c855bc2fd8f
Robert Haidl <haidlrob@gmail.com>
2018-10-30 00:39:20 +0100

    fixed particles

--- js/index.js
+++ js/index.js
@@ -1,28 +1,32 @@
 $(document).ready(function () {
-	callListener();
+    callListener();
 });
 
-function callListener(){
+function callListener() {
     loadParticles();
     callClickListener();
 }
 
-function loadParticles(){
-	particlesJS.load('landing', 'assets/particlesjs-config.json', function() {
-	});
+function loadParticles() {
+    particlesJS.load('landing', 'assets/particlesjs-config.json', function () {});
 }
 
-function callClickListener(){
-    $("#more_information").on("click", function(e){
+function callClickListener() {
+    $("#more_information").on("click", function (e) {
         alert("not implemented yet");
-	});
-	
+    });
 
-var options = {
-  strings: ["IT Lösungen", "Suchmaschinenoptimierung", "Webdesign", "Appdesign"],
-  typeSpeed: 200
+
+    var options = {
+        strings: ["IT Lösungen", "SEO", "Webdesign", "Appdesign"],
+        typeSpeed: 200,
+        loop: true,
+        loopCount: Infinity,
+        backSpeed: 150,
+        showCursor: false
+    }
+
+    var typed = new Typed(".typer", options);
+
+
 }
-
-var typed = new Typed(".typer", options);
-}
-
