1486eda4cb15fa585d7c9aa58660f0cc0e87b816
DESKTOP-1JKN9F0\tothm <mate.toth@stud.fh-campuswien.ac.at>
2018-11-09 01:14:39 +0100

    added new portfolio

--- js/portfolio.js
+++ js/portfolio.js
@@ -31,3 +31,23 @@ anime.timeline({
       return 150 * (i + 1)
     }
   });
+
+  // Wrap every letter in a span
+$('.ml10 .letters').each(function(){
+  $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+});
+
+anime.timeline({loop: false})
+  .add({
+    targets: '.ml10 .letter',
+    rotateY: [-90, 0],
+    duration: 1300,
+    delay: function(el, i) {
+      return 45 * i;
+    }
+  });
+
+  $(".page2, .page3, .page4, .page5 ").click(function() {
+    window.location = $(this).find("a").attr("href"); 
+    return false;
+  });
