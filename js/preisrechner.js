08bafa16d759dc669d21ec0ee4c26b4a24e746fe
DESKTOP-1JKN9F0\tothm <mate.toth@stud.fh-campuswien.ac.at>
2018-11-15 00:38:12 +0100

    icon changed

--- js/preisrechner.js
+++ js/preisrechner.js
@@ -9,14 +9,24 @@ function callListener(){
 
 function callClickListener(){
     $("#start").on("click", function () {
-        $("#page1").show();
+            $('#preisrechner-title').css('animation','fade 0.5s');
+            $('#preisrechner-title').css('opacity','0');
+            $(this).css('transform','scale(2)');
+            $(this).css('margin-top','-200px');
+            $(this).css('animation','move 2.5s');
+            $(this).css('animation-delay','1s');
+
+            setTimeout(() => {
+                $("#page1").fadeIn();
+                $("#start").hide();
+            }, 2500);
     });
 
     $(".pr-item").on("click", function () {
         let page = $(this).attr("forward");
         hideAllPages();
         console.log("Go to page:"+page);
-        $("#"+page).show();
+        $("#"+page).fadeIn();
     });
 }
 
