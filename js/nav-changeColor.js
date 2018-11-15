ecae9cd0dd4cf082d356aeff32723124651fb599
Robert Haidl <haidlrob@gmail.com>
2018-11-15 10:16:20 +0100

    added nav-changeColor

--- js/nav-changeColor.js
+++ js/nav-changeColor.js
@@ -1,11 +1,22 @@
 $(document).ready(function () {
     $(window).scroll(function () {
         var element1height = $(".wrapper").position().top;
+        var element1bottom = $(".wrapper").height()+element1height;
         var st = $(this).scrollTop();
         if (st < element1height) {
             $(".menuToggle").css("color", "white");
         } else {
             $(".menuToggle").css("color", "black");
         }
+        /*
+
+                if (st > element1bottom) {
+            $(".menuToggle").css("color", "white");
+        } else {
+            $(".menuToggle").css("color", "black");
+        }
+
+        */
+
     });
 });
