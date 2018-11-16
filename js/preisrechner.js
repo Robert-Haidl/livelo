f87d6d7458cf63936a0eaea282ead3f649f855b9
DESKTOP-1JKN9F0\tothm <mate.toth@stud.fh-campuswien.ac.at>
2018-11-16 06:56:17 +0100

    preisrechner

--- js/preisrechner.js
+++ js/preisrechner.js
@@ -13,14 +13,14 @@ function callClickListener() {
         $('#preisrechner-title').css('opacity', '0');
         $(this).css('transform', 'scale(2)');
         $(this).css('margin-top', '-200px');
-        $(this).css('animation', 'move 1.5s');
-        $(this).css('animation-delay', '.5s');
+        $(this).css('animation', 'move 2s');
+        $(this).css('animation-delay', '1s');
         setTimeout(() => {
             $(".wrapper").css("padding", "2em 0 4em 0");
             $("#preisrechner-title").hide();
             $("#page1").fadeIn();
             $("#start").hide();
-        }, 1200);
+        }, 1800);
     });
 
     $(".pr-item").on("click", function () {
