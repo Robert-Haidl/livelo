fa9519b97fe1cbd41d19dd364f3b17470f2c8585
Robert Haidl <haidlrob@gmail.com>
2018-11-21 14:13:55 +0100

    created impressum

---
+++ js/cookiescript.js
@@ -0,0 +1,20 @@
+window.addEventListener("load", function(){
+window.cookieconsent.initialise({
+  "palette": {
+    "popup": {
+      "background": "#edeff5",
+      "text": "#838391"
+    },
+    "button": {
+      "background": "#4b81e8"
+    }
+  },
+  "theme": "classic",
+  "position": "bottom-right",
+  "content": {
+    "message": "Diese Website benutzt Cookies.",
+    "dismiss": "Verstanden",
+    "link": "Mehr",
+    "href": "https://livelo.at/datenschutz"
+  }
+})});
