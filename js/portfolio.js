35410a90a2e83a8e5c4701586d15e812b12e38ff
DESKTOP-1JKN9F0\tothm <mate.toth@stud.fh-campuswien.ac.at>
2018-11-25 12:22:47 +0100

    portfolio half done

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,11 +1,26 @@
-  $(document).ready(function() {
-    ...
-    $(".inner").onepage_scroll({
-       sectionContainer: "section", 
-       easing: "ease",
-       animationTime: 1000,
-       pagination: true,
-       updateURL: false
-    });
-    ...
-    });
+$(document).ready(function () {
+  hideAllPages();
+  callListener();
+  hideStart();
+});
+    
+$(".pr-item").on("click", function () {
+  let page = $(this).attr("forward");
+  hideStart();
+  hideAllPages();
+  $("#" + page).fadeIn();
+});
+
+function hideAllPages() {
+  $(".page").hide();
+}
+
+function callListener() {
+  callClickListener();
+}
+
+function hideStart(){
+  $(".pagestart").hide();
+}
+
+$(".firstbutton, .lastbutton").off("click");
